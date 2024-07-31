c37212076a91399c0b2f458fb64f5623f44fc659
NeilMagon <neil.magon97@gmail.com>
2024-07-31 16:21:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Holiday from './component/Holiday'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <section className='mt-3'>
+        <Holiday />
+      </section>
+    </div>
+  )
+}
+
+export default App
